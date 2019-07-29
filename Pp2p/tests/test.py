f41f201ec79f0aff2c660955e91c152830c58f09
CyrileVerdeyen <cyrile.verdeyen@gmail.com>
2019-07-29 14:49:14 +0100

    Makes a random group of data from all the batches

--- Pp2p/tests/test.py
+++ Pp2p/tests/test.py
@@ -20,12 +20,23 @@ class CO():
 
 class node():
     def __init__(self):
-        batch = random.randint(1, 5)
+
+        imgs = {b"data": [], b"labels": []}
+
+        for i in (range (1,6)):
+            data = random.randint(0, 4999)
+            img = unpickle("./cifar-10-batches-py/data_batch_" + str(i))
+            for j in range(5000):
+                imgs[b"data"].append(img[b"data"][(data+j)])
+                imgs[b"labels"].append(img[b"labels"][(data+j)])
+
+        print(len(imgs[b"data"]))
+
+
 
         self.DEFAULT_PORT = 5006
         self.BOOTSRAP_NODES = ["localhost:5005"]
 
-        imgs = unpickle("./cifar-10-batches-py/data_batch_" + str(batch))
         self.ml = ml.mlsvm(imgs, "0")
 
     def run(self):
